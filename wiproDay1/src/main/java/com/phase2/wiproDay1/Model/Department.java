1045eb8bcf8809a4b818a73d2b4fe984cf96bb83
Satyam-Nayak <satyamn120@gmail.com>
2025-02-17 14:38:49 +0530

    drpartment model added

--- wiproDay1/src/main/java/com/phase2/wiproDay1/Model/Department.java
+++ wiproDay1/src/main/java/com/phase2/wiproDay1/Model/Department.java
@@ -1,4 +1,33 @@
 package com.phase2.wiproDay1.Model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "departments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "college_id", nullable = false)
+    private College college;
+
+    @OneToMany(mappedBy = "department", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Faculty> faculties;
+
+    @OneToOne
+    @JoinColumn(name = "hod_id")
+    private Faculty hod;
 }
